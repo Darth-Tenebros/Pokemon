887db5087a4754a03dbd01bf11b8d67667da5b4f
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-05-27 14:09:26 +0200

    models

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -1,8 +1,11 @@
-namespace PokemonReviewApp.Models{
+namespace PokemonReviewApp.Models
+{
 
-    public class Pokemon{
+    public class Pokemon
+    {
         public int Id {get; set;}
         public string Name {get; set;}
         public DateTime BirthDate {get; set;}
+        public ICollection<Review> Reviews  {get; set;}
     }
 }
