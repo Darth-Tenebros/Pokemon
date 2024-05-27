3229f1e14a2ca1d5d0d171f230a2832f54d44441
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-05-27 16:40:43 +0200

    added rating field

--- Models/Review.cs
+++ Models/Review.cs
@@ -5,7 +5,9 @@ namespace PokemonReviewApp.Models
         public int Id {get; set;}
         public string Title {get; set;}
         public string Text {get; set;}
+        public int Rating { get; internal set; }
         public Reviewer Reviewer {get; set;}
         public Pokemon Pokemon {get; set;}
+        
     }
 }
