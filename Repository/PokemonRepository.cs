8acce9dd2f8c4dd8261ca020b050fcab7c1ba094
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-02 22:23:10 +0200

    IPokemon impl

--- Repository/PokemonRepository.cs
+++ Repository/PokemonRepository.cs
@@ -16,9 +16,36 @@ namespace PokemonReviewApp.Repository
             _context = context;
         }
 
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemon.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemon.Where(p => p.Name.Equals(name)).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokemonId)
+        {
+           var review = _context.Reviews.Where(p => p.Id == pokemonId);
+           
+           if(review.Count() <= 0)
+           {
+                return 0;
+           }
+
+           return (decimal)review.Sum(rating => rating.Rating) / review.Count();
+        }
+
         public ICollection<Pokemon> GetPokemons()
         {
             return _context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+
+        public bool PokemonExists(int pokemonId)
+        {
+            return _context.Pokemon.Any(p => p.Id == pokemonId);
         }
     }
 }
