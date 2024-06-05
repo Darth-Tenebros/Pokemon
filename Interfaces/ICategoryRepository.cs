77479a4bb0fb2fb42f58a8be812884130b07b3c1
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-05 20:09:47 +0200

    ICategoryRepo

---
+++ Interfaces/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int id);
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+        bool CategoryExists(int id);
+    }
+
+}
