a2fb5b01977e4205863a6289329edd635f4c0e22
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-06 21:59:37 +0200

    countryrepo impl

---
+++ Repository/CountryRepository.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly DataContext _context;
+        public CountryRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool CountryExists(int countryId)
+        {
+            return _context.Countries.Any(c => c.Id == countryId);
+        }
+
+        public ICollection<Country> GetCountries()
+        {
+            return _context.Countries.OrderBy(c => c.Id).ToList();
+        }
+
+        public Country GetCountry(int id)
+        {
+           return _context.Countries.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public Country GetCountryByOwner(int ownerId)
+        {
+            return _context.Owners
+                .Where(o => o.Id == ownerId)
+                .Select(c => c.Country).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnersFromCountry(int countryId)
+        {
+            return _context.Owners.Where(o => o.Country.Id == countryId).ToList();
+        }
+    }
+}
