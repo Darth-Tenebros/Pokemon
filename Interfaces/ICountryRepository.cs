bcd8497202419d257aecc2ca2889eb7bcf6ce71d
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-06 21:59:13 +0200

    Icountry..

---
+++ Interfaces/ICountryRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountry(int id);
+        Country GetCountryByOwner(int ownerId);
+        bool CountryExists(int countryId);
+
+        ICollection<Owner> GetOwnersFromCountry(int countryId);
+        
+    }
+}
