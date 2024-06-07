a067d608ca8aa35d04f05756257cbf441ca5a008
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-07 20:16:06 +0200

    IReview..

---
+++ Interfaces/IReviewRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IReviewRepository
+    {
+        ICollection<Review> GetReviews();
+        Review GetReview(int reviewId);
+        ICollection<Review> GetReviewsOfAPokemon(int pokeId);
+        bool ReviewExists(int reviewId);
+    }
+}
