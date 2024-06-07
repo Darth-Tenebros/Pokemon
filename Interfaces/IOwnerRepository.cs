9641650d3da8ff6e146d367ac360475083da1d7a
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-07 20:15:09 +0200

    IOwner..

---
+++ Interfaces/IOwnerRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IOwnerRepository
+    {
+        ICollection<Owner> GetOwners();
+        Owner GetOwner(int id);
+        ICollection<Owner> GetOwnerOfAPokemon(int pokeId);
+        ICollection<Pokemon> GetPokemonByOwner(int ownerId);
+        bool OwnerExists(int ownerId);
+    }
+}
